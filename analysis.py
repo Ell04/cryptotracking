f15bc6f2b792b7adfbdf6d37747ebcd848dd8235
Ell <elliottleek@icloud.com>
2025-05-22 16:09:31 +0100

    KMeans clustering and Silhouette score

--- analysis.py
+++ analysis.py
@@ -1,15 +1,23 @@
-import pandas as pd
-import numpy as np
 import data 
 import requests
-from gdeltdoc import GdeltDoc, Filters, near, repeat, multi_repeat
 import json
 import os
-from tqdm import tqdm
 import utils
+import datetime
+
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-import datetime
+from babel.numbers import format_currency
+from gdeltdoc import GdeltDoc, Filters, near, repeat, multi_repeat
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score, silhouette_samples
+
+from datetime import timedelta
 
 '''
 
@@ -21,20 +29,15 @@ import datetime
 
 def unix_to_datetime(series):
 
-    timestampvalues = [timestamp[0] for timestamp in series]
-    data_df = pd.DataFrame(series.tolist())
-    
-    for time in timestampvalues:
-        timestampseconds = time / 1000
-        datetimevalue = datetime.datetime.fromtimestamp(timestampseconds)
+    print(series)
 
-        data_df = data_df.replace(to_replace=time, value=datetimevalue)
+    datetimevalues = [datetime.datetime.fromtimestamp(t / 1000).strftime("%Y-%m-%d") for t in series]
+
+    print("Datetime values: ", datetimevalues)
+    return datetimevalues
 
 
-    print(data_df)
-
-
-def plot(prices, volumes, market_caps):
+def plotrawdata(prices, volumes, market_caps):
     
     ## -- Prices data --
     pricevalue = [price[1] for price in prices]
@@ -47,26 +50,143 @@ def plot(prices, volumes, market_caps):
     plt.legend()
     plt.show()
 
-def anomaly_pattern_detection(datapath):
-
-    ## -- Get the data from the JSON file into a pandas dataframe (90 entries for 90 days) --
-    data_df = pd.read_json(datapath)
-
-    ## -- Ensure correct information --
-    assert data_df.shape[0] == 91, "Dataframe does not contain 90 entries / information for 90 day period! Please check the data.py."
-    assert data_df.shape[1] == 3, "Dataframe missing information for one of or multiple of prices, market caps and total volumes."
-
-    print(data_df)
-
-    ## -- Get the price, volume and market caps data as seperate dataframes --
-    prices = data_df["prices"]
-    volumes = data_df["total_volumes"]
-    market_caps = data_df["market_caps"]
-
-    ## -- Plot some values --
-    plot(prices, volumes, market_caps)
-
+    ## -- Volume data --
+    volumevalue = [volume[1] for volume in volumes]
+    y = list(volumevalue)
     
+    plt.plot(y, color='red', label='Volume data for 90 days')
+    plt.title("Raw Volume Data")
+    plt.xlabel("Days")
+    plt.ylabel("Volume")
+    plt.legend()
+    plt.show()
+
+    ## -- Market cap data --
+    marketcapvalue = [mcap[1] for mcap in market_caps]
+    y = list(marketcapvalue)
+    
+    plt.plot(y, color='red', label='Market cap data for 90 days')
+    plt.title("Raw Market Cap Data")
+    plt.xlabel("Days")
+    plt.ylabel("Market Cap")
+    plt.legend()
+    plt.show()
+
+
+def cluster(data, n_clusters):
+    values = [v[1] for v in data]
+    valuelist = np.array(values)
+
+    scaler = StandardScaler()
+    valuelist = scaler.fit_transform(valuelist.reshape(-1, 1))
+
+    kmeanscluster = KMeans(n_clusters=n_clusters, random_state=0, n_init='auto').fit(valuelist)
+
+    labels = kmeanscluster.labels_
+    centroids = kmeanscluster.cluster_centers_
+
+    return labels, centroids, scaler, valuelist
+
+def silhouette_metric(data, n_clusters):
+    SILHOUETTE_THRESHOLD = 0.2
+
+    labels, centroids, scaler, valuelist = cluster(data, n_clusters)
+    silhouette = silhouette_score(valuelist, labels, metric='euclidean')
+    silhouette_s = silhouette_samples(valuelist, labels, metric='euclidean')
+
+    anomalous_indicies = np.where(silhouette_s <= SILHOUETTE_THRESHOLD)[0]
+
+    return silhouette, anomalous_indicies, labels, centroids, scaler, valuelist
+
+def anomaly_pattern_detection(datapath, showplots=False, priceanomalies = [], volumeanomalies = [], marketcapanomalies = []):
+
+    prices, volumes, market_caps, \
+    maxprice, minprice, stddevprice, varprice, avgprice, \
+    maxvolume, minvolume, stddevvolume, varvolume, avgvolume, \
+    maxmarketcap, minmarketcap, stddevmarketcap, varmarketcap, avgmarketcap = utils.getdata(datapath)
+    
+    ## -- Plot some values and basic metrics --
+    print("-----------------------------\nMaximum price: ", format_currency(maxprice, 'USD', locale='en_US'))
+    print("Minimum price: ", format_currency(minprice, 'USD', locale='en_US'))
+    print("Standard deviation of prices: ", format_currency(stddevprice, 'USD', locale='en_US'))
+    print("Variance of prices: ", format_currency(varprice, 'USD', locale='en_US'))
+    print("Average price: ", format_currency(avgprice, 'USD', locale='en_US'), "\n-----------------------------")
+
+
+    print("Maximum volume: ", format_currency(maxvolume, 'USD', locale='en_US'))
+    print("Minimum volume: ", format_currency(minvolume, 'USD', locale='en_US'))
+    print("Standard deviation of volumes: ", format_currency(stddevvolume, 'USD', locale='en_US'))
+    print("Variance of volumes: ", format_currency(varvolume, 'USD', locale='en_US'))
+    print("Average volume: ", format_currency(avgvolume, 'USD', locale='en_US'), "\n-----------------------------")
+
+    print("Maximum market cap: ", format_currency(maxmarketcap, 'USD', locale='en_US'))
+    print("Minimum market cap: ", format_currency(minmarketcap, 'USD', locale='en_US'))
+    print("Standard deviation of market cap: ", format_currency(stddevmarketcap, 'USD', locale='en_US'))
+    print("Variance of market cap: ", format_currency(varmarketcap, 'USD', locale='en_US'))
+    print("Average market cap: ", format_currency(avgmarketcap, 'USD', locale='en_US'), "\n-----------------------------")
+
+    plotrawdata(prices, volumes, market_caps) if showplots else None
+
+    ## -- Detect anomalies in the data --
+    ## -- Prices --
+
+    pricesilhouette, anomalous_indicies_prices, _, _, _, _ = silhouette_metric(prices, n_clusters=3) 
+
+    print("Good silhouette score for price: ", pricesilhouette) if pricesilhouette > 0.5 else None
+    print("Bad silhouette score for price clustering: ", pricesilhouette) if pricesilhouette <= 0.2 else None
+
+    print("Anomalies in price data at index: ", anomalous_indicies_prices, "\n") if len(anomalous_indicies_prices) > 0 else print("No anomalies in price data!\n")
+
+    ## -- Volumes --
+
+    volumesilhouette, anomalous_indicies_volumes, _, _, _, _= silhouette_metric(volumes, n_clusters=3)
+
+    print("Good silhouette score for volume: ", volumesilhouette) if volumesilhouette > 0.5 else None
+    print("Bad silhouette score for volume clustering: ", volumesilhouette) if volumesilhouette <= 0.2 else None
+
+    print("Anomalies in volume data at index: ", anomalous_indicies_volumes, "\n") if len(anomalous_indicies_volumes) > 0 else print("No anomalies in volume data!\n")
+
+    ## -- Market caps --
+
+    marketsilhouette, anomalous_indicies_mcaps, _, _, _, _= silhouette_metric(market_caps, n_clusters=3)
+
+    print("Good silhouette score for market cap: ", marketsilhouette) if marketsilhouette > 0.5 else None
+    print("Bad silhouette score for market cap clustering: ", marketsilhouette) if marketsilhouette <= 0.2 else None
+
+    print("Anomalies in market cap data at index: ", anomalous_indicies_mcaps, "\n") if len(anomalous_indicies_mcaps) > 0 else print("No anomalies in market cap data!\n")
+
+    if len(anomalous_indicies_prices) > 0:
+        anomalytime = [prices[i][0] for i in anomalous_indicies_prices]
+
+        anomalyreadabletime = unix_to_datetime(anomalytime)
+        print("Anomalies detected at time: ", anomalyreadabletime, " for price data")
+
+        priceanomalies.append(anomalyreadabletime)
+
+    if len(anomalous_indicies_volumes) > 0:
+        anomalytime = [volumes[i][0] for i in anomalous_indicies_volumes]
+
+        print(volumes[7][0])
+
+        anomalyreadabletime = unix_to_datetime(anomalytime)
+        print("Anomalies detected at time: ", anomalyreadabletime, " for volume data")
+
+        volumeanomalies.append(anomalyreadabletime)
+
+    if len(anomalous_indicies_mcaps) > 0:
+        anomalytime = [market_caps[i][0] for i in anomalous_indicies_mcaps]
+
+        anomalyreadabletime = unix_to_datetime(anomalytime)
+        print("Anomalies detected at time: ", anomalyreadabletime, " for market cap data")
+
+        marketcapanomalies.append(anomalyreadabletime)
+
+    else:
+        print("No anomalies detected in the data!")
+
+
+
+
 def gdelt_query(filters, storejson=None):
 
     gd = GdeltDoc()
@@ -87,16 +207,16 @@ def gdelt_query(filters, storejson=None):
 
     print("Relevant articles and timelines saved!")
 
-def getqueryparameters():
+def getqueryparameters(anomalytime):
     
-    ## -- TODO, 
-    ## -- (1) Pass in dates of timeseries of anomaly --  
-    ## -- (2) Pass in bitcoin or ethereum in case --
+    ## -- Check for events in the past week --
+    anomalytimestart = datetime.datetime.strptime(anomalytime[0], "%Y-%m-%d") - timedelta(days=7)
+    anomalytimestop = datetime.datetime.strptime(anomalytime[0], "%Y-%m-%d") + timedelta(days=7)
 
     filters = Filters(
         keyword="bitcoin OR ethereum",
-        start_date="2023-01-01",
-        end_date="2023-12-31",
+        start_date=anomalytimestart,
+        end_date=anomalytimestop,
         country=["UK", "US"]
         #tone=">5"
         #domain=["bbc.co.uk", "nytimes.com", "theguardian.com"],
@@ -114,12 +234,9 @@ def getqueryparameters():
 
 
 if __name__ == "__main__":
+    os.system("CLS")
 
     datapath = os.path.join(utils.getdirs(), "data/bitcoin_data.json")
-
     data_series = pd.read_json(datapath)
 
-    unix_to_datetime(data_series['prices'])
     anomaly_pattern_detection(str(datapath))
-    filters = getqueryparameters()
-    gdelt_query(filters, storejson=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 import os
+import numpy as np
+import pandas as pd
 
 def dumpjson(data, filename):
     
@@ -9,3 +11,42 @@ def dumpjson(data, filename):
 def getdirs():
 
     return os.getcwd()
+    
+
+def getdata(datapath):
+    
+    ## -- Get the data from the JSON file into a pandas dataframe (90 entries for 90 days) --
+    data_df = pd.read_json(datapath)
+
+    ## -- Ensure correct information --
+    assert data_df.shape[0] == 91, "Dataframe does not contain 90 entries / information for 90 day period! Please check the data.py."
+    assert data_df.shape[1] == 3, "Dataframe missing information for one of or multiple of: prices, market_caps, total_volumes."
+
+    ## -- Get the price, volume and market caps data as seperate dataframes --
+    prices = data_df["prices"]
+    volumes = data_df["total_volumes"]
+    market_caps = data_df["market_caps"]
+
+    pricesvalues = [price[1] for price in prices]
+    volumesvalues = [volume[1] for volume in volumes]
+    marketcapsvalues = [marketcap[1] for marketcap in market_caps]
+
+    maxprice = max(pricesvalues)
+    minprice = min(pricesvalues)
+    stddevprice = np.std(pricesvalues)
+    varprice = np.var(pricesvalues)
+    avgprice = np.mean(pricesvalues)
+
+    maxvolume = max(volumesvalues)
+    minvolume = min(volumesvalues)
+    stddevvolume = np.std(volumesvalues)
+    varvolume = np.var(volumesvalues)
+    avgvolume = np.mean(volumesvalues)
+
+    maxmarketcap = max(marketcapsvalues)
+    minmarketcap = min(marketcapsvalues)
+    stddevmarketcap = np.std(marketcapsvalues)
+    varmarketcap = np.var(marketcapsvalues)
+    avgmarketcap = np.mean(marketcapsvalues)
+
+    return prices, volumes, market_caps, maxprice, minprice, stddevprice, varprice, avgprice, maxvolume, minvolume, stddevvolume, varvolume, avgvolume, maxmarketcap, minmarketcap, stddevmarketcap, varmarketcap, avgmarketcap
